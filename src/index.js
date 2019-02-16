8936b91489668e95bb9cdba45e3434087538024f
ianDesson <ian.desson@outlook.com>
2019-02-16 17:03:43 -0500

    Removed express stuff

--- src/index.js
+++ src/index.js
@@ -11,24 +11,3 @@ ReactDOM.render(<App />, document.getElementById('root'));
 // Learn more about service workers: http://bit.ly/CRA-PWA
 serviceWorker.unregister();
 
-// Express Stuff
-const express = require('express');
-require('dotenv').config();
-
-const app = express();
-
-const port = process.env.PORT || 5000;
-
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
-
-app.use((req, res, next) => {
-  res.send('Welcome to Express');
-});
-
-app.listen(port, () => {
-  console.log(`Server running on port ${port}`)
-});
