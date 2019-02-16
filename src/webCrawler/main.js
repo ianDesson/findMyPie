807ccca2883ed063375ce54ea77ff770e27c5dfb
Alex Tasseron <31492559+theangrytoast@users.noreply.github.com>
2019-02-16 15:47:33 -0500

    Update main.js

--- src/webCrawler/main.js
+++ src/webCrawler/main.js
@@ -3,15 +3,26 @@ var Crawler = require("crawler");
 var testCrawler = new Crawler({
   maxConnections : 10,
   callback : function (error, res, done) {
-    if(error){
-      console.log(error);
-    }else{
-      var $ = res.$;
-      //Cheerio
-      console.log($("inserttitle").text());
+    //Note:
+    //console.log(error);
+    //console.log(result);
+    //console.log($);
+    //console.log(result.body);
+
+    if (result){
+      var page = result.body;
+      var res = page.match(/price/i);
+      if (res && res.length > 0){
+        console.log(result.body);//prints boday where a hit is found
+      }
     }
-    done();
+
+    $("a").each(function(index, a) {//looks for anchor tags and resubmits new
+      //page to the queue
+      console.log(a.href);
+      c.queue(a.href);//logs each URL where a hit is found
+    });
   }
 });
 
-testCrawler.queue('https://gabrielpizza.com/restaurant-menu/pizza/');
+testCrawler.queue('https://gabrielpizza.com/');
