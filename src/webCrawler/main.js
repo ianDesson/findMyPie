b9c26c771923eb3155ef2bdee7b7e96b9b2f5e25
vishalghaie <vishalghaie@hotmail.com>
2019-02-17 10:31:08 -0500

    re-did webCrawler

--- src/webCrawler/main.js
+++ src/webCrawler/main.js
@@ -1,29 +1,44 @@
-var $ = require('jQuery');
 var Crawler = require("crawler");
 
-var testCrawler = new Crawler({
-  maxConnections : 10,
-  callback: function (error, res, done) {
-    //Note:
-    //console.log(error);
-    //console.log(result);
-    //console.log($);
-    //console.log(result.body);
-
-    if (res){
-      var page = res.body;
-      var res = page.match(/price/i);
-      if (res && res.length > 0){
-        console.log(res.body);//prints boday where a hit is found
-      }
+var c = new Crawler({
+    maxConnections : 10,
+    // This will be called for each crawled page
+    callback : function (error, res, done) {
+        if(error){
+            console.log(error);
+        }else{
+            var $ = res.$;
+            // $ is Cheerio by default
+            //a lean implementation of core jQuery designed specifically for the server
+            console.log($("title").text());
+        }
+        done();
     }
-
-    $("a").each(function(index, a) {//looks for anchor tags and resubmits new
-      //page to the queue
-      console.log(a.href);
-      c.queue(a.href);//logs each URL where a hit is found
-    });
-  }
 });
 
-testCrawler.queue('https://gabrielpizza.com/');
+// Queue just one URL, with default callback
+c.queue('http://www.amazon.com');
+
+// Queue a list of URLs
+c.queue(['http://www.google.com/','http://www.yahoo.com']);
+
+// Queue URLs with custom callbacks & parameters
+c.queue([{
+    uri: 'http://parishackers.org/',
+    jQuery: false,
+
+    // The global callback won't be called
+    callback: function (error, res, done) {
+        if(error){
+            console.log(error);
+        }else{
+            console.log('Grabbed', res.body.length, 'bytes');
+        }
+        done();
+    }
+}]);
+
+// Queue some HTML code directly without grabbing (mostly for tests)
+c.queue([{
+    html: '<p>This is a <strong>test</strong></p>'
+}]);
